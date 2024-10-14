a4ab6c52cbc05c168118c2bf6cb7634f859e1c27
nane984 <br.scekic@gmail.com>
2024-10-14 07:39:50 -0400

    Task 5 getBook review

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -58,8 +58,12 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if(isbn){
+    let book_rev = books[isbn];
+    return res.status(200).send(JSON.stringify(book_rev, null, 4));
+  }
+  return res.status(200).json({message: "Book not found"});
 });
 
 module.exports.general = public_users;
